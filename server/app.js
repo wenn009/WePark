8ba65382806b34b601d6fa69226c14f8455672e8
Xiaowen Huang <wenn009@gmail.com>
2017-10-01 00:02:40 -0400

    provide starter code for server

---
+++ server/app.js
@@ -0,0 +1,18 @@
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var express = require('express');
+var path = require('path');
+
+const PORT = process.env.PORT || 8000;
+
+var app = express();
+//allow cross origin
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json({"hi":"wep park"});
+  });
+
+app.listen(PORT, () => {
+    console.log('server is running on port: ' + PORT);
+});
