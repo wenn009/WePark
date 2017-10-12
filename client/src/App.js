6234010706969edc8084cb89e31ca0b1245f8452
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-10-12 18:06:45 -0400

    Removed React default homepage code

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import NavBar from './NavBar.js';
 class Footer extends Component {
   render() {
       return(
-          <footer id="foot">
+          <footer>
               WePark Inc.
           </footer>
       );
@@ -17,13 +17,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <NavBar />
           This is where the map goes
         <Footer />
