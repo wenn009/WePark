d7589d55aa21a67a049d3b0fd40781afdff940a0
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-10-13 00:13:35 -0400

    Removed old test map code

--- client/src/Map.js
+++ client/src/Map.js
@@ -1,19 +1,6 @@
 import React, { Component } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
-class Map extends Component {
-    render() {
-        return(
-            <GoogleMap
-                defaultZoom={8}
-                defaultCenter={ {lat: -34.397, lng: 150.644} }
-            >
-            {this.props.isMarkerShown && <Marker position={ { lat: -34.397, lng: 150.644 } } />}
-            </GoogleMap> 
-        );
-    }
-}
-
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
     <GoogleMap
         defaultZoom={16}
