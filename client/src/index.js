66f7acfd9ab84ffe08ffab0f1fb7ee1706b6870f
Xiaowen Huang <wenn009@gmail.com>
2017-10-20 00:07:12 -0400

    add login page

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import LoginPage from './logIn/loginPage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<LoginPage />, document.getElementById('root'));
 registerServiceWorker();
