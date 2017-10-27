15facb90b63e13600380188f02c3c3f41ac8355a
Xiaowen Huang <wenn009@gmail.com>
2017-10-27 00:05:15 -0400

    add signup page

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import LoginPage from './logIn/loginPage';
+import SignupPage from './SignUp/signupPage';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<LoginPage />, document.getElementById('root'));
+ReactDOM.render(<SignupPage />, document.getElementById('root'));
 registerServiceWorker();
