d9f72470f8be4bb6f5ff22ee9e8b2c686bba49e0
Xiaowen Huang <wenn009@gmail.com>
2017-10-27 10:27:49 -0400

    add login controller

--- server/models/users.js
+++ server/models/users.js
@@ -1,13 +1,26 @@
+const bcrypy = require('bcrypt-nodejs');
+
 module.exports = (sequelize, DataTypes) => {
-    const Users = sequelize.define('Users',{
+    const Users = sequelize.define('Users', {
         FirstName: DataTypes.STRING,
         LastName: DataTypes.STRING,
         UserType: DataTypes.STRING,
         PhoneNumber: DataTypes.STRING,
         Address: DataTypes.STRING,
-        Email: DataTypes.STRING(320)
+        Email: DataTypes.STRING(320),
+        password_hash: DataTypes.STRING
     });
 
+    Users.beforeCreate((user) =>
+        new sequelize.Promise((resolve) => {
+            bcrypt.hash(user.password_hash, null, null, (err, hashedPassword) => {
+                resolve(hashedPassword);
+            });
+        })
+            .then((hashedPassword) => {
+                user.password_hash = hashedPassword;
+            })
+    );
     Users.associate = (models) => {
         models.Users.hasMany(models.Garages);
     }
