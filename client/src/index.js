26eed28152852a4c2df9f23e4194bfa6e9c66b17
Xiaowen Huang <wenn009@gmail.com>
2017-11-03 14:05:51 -0400

    modify index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,21 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Login from './logIn/loginPage';
+import Signup from './SignUp/signupPage';
 
 import AppRoute from './AppRoute';
 
 //ReactDOM.render(<AppRoute />, document.getElementById('root'));
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NavBar from './NavBar';
 import GarageListContainer from './garageList';
 
 ReactDOM.render(
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route exact path='/' component={App} />
             <Route exact path='/list' component={GarageListContainer} />
-        </div>
+            <Route path='/login' exact component={Login}/>
+            <Route path='/signup' exact component={Signup}/>
+        </Switch>
     </BrowserRouter>, 
     document.getElementById('root')
 );
