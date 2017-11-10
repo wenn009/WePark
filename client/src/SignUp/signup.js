b309e0083ea3758dbd9708c2b8add581801e335e
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-11-10 14:14:42 -0500

    Added names to form inputs

--- client/src/SignUp/signup.js
+++ client/src/SignUp/signup.js
@@ -9,15 +9,21 @@ class SignUpForm extends Component {
                 <div className="panel-body">
                     <form className="form-horizontal">
                         <div className="form-group">
-                            <label className="col-sm-2 control-label">Name: </label>
+                            <label className="col-sm-2 control-label">First Name: </label>
                             <div className="col-sm-10">
-                                <input type="text" className="form-control" />
+                                <input type="text" name="FirstName" className="form-control" />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label className="col-sm-2 control-label">Last Name: </label>
+                            <div className="col-sm-10">
+                                <input type="text" name="LastName" className="form-control" />
                             </div>
                         </div>
                         <div className="form-group">
                             <label className="col-sm-2 control-label">Email: </label>
                             <div className="col-sm-10">
-                                <input type="email" className="form-control" />
+                                <input type="email" name="Email" className="form-control" />
                             </div>
                         </div>
                         <div className="form-group">
