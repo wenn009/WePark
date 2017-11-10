0c92bc5a686a63ecaa7945f728d2c7dba99b654b
Ekko Lin <sihansolotop@gmail.com>
2017-11-10 14:38:41 -0500

    Timesheet controller fixed

--- server/controllers/timesheet.js
+++ server/controllers/timesheet.js
@@ -6,27 +6,39 @@ const TimeSheetController = {
     const router = express.Router();
 
     router.get("/:id", this.index); // Fetch particular timesheet
+    router.post("/", this.createTimeSheet); // Create a time sheet
     router.post("/:id/timeSlots", this.scheduleTimeSlot); // Create new time slot under a timesheet
-    router.put("/:id/timeSlots", this.updateTimeSlot);  // Update a time slot under a time sheet
+    router.put("/:id/timeSlots", this.updateTimeSlot); // Update a time slot under a time sheet
     router.delete("/:id/timeSlots", this.deleteTimeSlot); // Delete a particular time slot by id
 
     return router;
   },
   index(req, res) {
-    models.timeSheet.findById(req.params.id),
-      {
+    models.timeSheet
+      .findById(req.params.id, {
         include: [
           {
             model: models.timeSlot
           }
         ]
-      }
-        .then(timesheet => {
-          res.json(timesheet).send("Received all time slots");
-        })
-        .catch(() => {
-          res.status(404).send("Get timesheet successfully");
-        });
+      })
+      .then(timesheet => {
+        res.json(timesheet).send("Received all time slots");
+      })
+      .catch(() => {
+        res.status(404).send("Get timesheet successfully");
+      });
+  },
+  createTimeSheet(req, res) {
+    models.timeSheet
+      .create({})
+      .then(timeSheet => {
+        res.json(timeSheet).send("Create timessheet successfully");
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(404).send("Can't create timesheet");
+      });
   },
   scheduleTimeSlot(req, res) {
     models.timeSheet
@@ -41,6 +53,7 @@ const TimeSheetController = {
         res.json(timeslot).send("Create successfully");
       })
       .catch(error => {
+        console.log(error);
         res.status(404).send("Can't create");
       });
   },
