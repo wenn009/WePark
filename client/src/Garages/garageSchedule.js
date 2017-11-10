8785b385485d058c8ab356d42e4d227600fb26e0
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-11-09 23:41:56 -0500

    Created scheduling page

--- client/src/Garages/garageSchedule.js
+++ client/src/Garages/garageSchedule.js
@@ -22,23 +22,68 @@ class GarageData extends Component {
             .then( jsonBody => {
                 this.setState({
                     address: jsonBody.Address,
+                    renting_price: jsonBody.Renting_Price,
+                    size: jsonBody.Size,
+                    date: jsonBody.createdAt,
+                    user: jsonBody.UserId,
                 });
             })
     }
 
     render() {
         return(
-            <h1>{ this.state.address }</h1>
+            <div className="panel panel-default">
+                <div className="panel-body">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xs-12 col-sm-6 col-md-3">
+                                <a href="#" className="thumbnail">
+                                    <img src="http://weknowyourdreams.com/images/house/house-04.jpg" className="img-fluid" />
+                                </a>
+                            </div>
+                            <div className="col-xs-12 col-sm-12 col-md-9">
+                                Address: { this.state.address } <br />
+                                Price: { this.state.renting_price } <br />
+                                Size: {this.state.size } <br />
+                                Posted: { this.state.date } <br />
+                                User: { this.state.user }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
 
-export default class GarageSchedule extends Component {
+class GarageSchedule extends Component {
+    render() {
+        return(
+            <div className="panel panel-default">
+                <div className="panel-body">
+                    <h3>Schedule List</h3>
+                    <h5>The availability will show here</h5>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default class GarageScheduleContainer extends Component {
     render() {
         return(
             <div>
-                <NavBar />
-                <GarageData idNumber={ this.props.match.params.number } />
+                <NavBar/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 col-sm-12">
+                            <GarageSchedule />
+                        </div>
+                        <div className="col-md-6 col-sm-12">
+                            <GarageData idNumber={ this.props.match.params.number } />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
